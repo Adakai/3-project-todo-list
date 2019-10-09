0108808f4542a09585ff7c0a1c105508fd4e2687
Adakai <abovethenegativity23@gmail.com>
2019-10-08 20:13:22 -0600

    able to add to list and to delete to list and array as well.

--- js/index.js
+++ js/index.js
@@ -1,43 +1,19 @@
-const dateElement = document.getElementById('date');
 // const listDisplay = document.getElementById('display_list');
 // const itemInput = document.getElementById('item_input');
 // const addItem = document.getElementById('add_item');
-
 const checked = './img/icons/checked.svg'
 const unchecked = './img/icons/unchecked.svg'
+
+const dateElement = document.getElementById('date');
 
 const dateOptions = {weekday: 'long', month:'short', day:'numeric'};
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString('en-US', dateOptions);
 
-
-// function addToDo(todo) {
-//     let item = `<li class="item">
-//                  <img src=${unchecked} job="complete">
-//                  <p class="text">${todo}</p>
-//                  <img id="trash" src="./img/icons/trash.svg" job="delete">
-//                  </li>`
-//     let position = 'beforeend';
-
-//     listDisplay.insertAdjacentHTML(position, item);
-// };  
-
-// document.addEventListener('keyup', function(event) {
-//     if (event.keyCode == 13) {
-//         let todo = itemInput.value;
-
-//         if (todo) {
-//             addToDo(todo);
-//             itemInput.value = '';
-//         }  
-//     }
-// });
-
-
 //jquery
 
-let biglist = [];
+let listItems = [];
 
 $("#display_list").sortable({
     stop: function(event, ui) {executecode();}
@@ -62,25 +38,31 @@ function addItem() {
     let tempobj = {};
     tempobj.listname = input;
     tempobj.liscol = [];
-    biglist.push(tempobj);
+    listItems.push(tempobj);
     $('#item_input').val('');
     printmypage();
-    console.log(biglist);
+    console.log(listItems);
 };
 
 function printmypage() {
     $('#display_list').html('');
-    for(var i = 0; i < biglist.length; i ++) {
+    for(var i = 0; i < listItems.length; i ++) {
         $('#display_list').append(`<li class="item">
                                         <img src=${unchecked}>
-                                        <p class="text">${biglist[i].listname}</p>
-                                        <img id="trash" src="./img/icons/trash.svg" onclick="trashClicked(this)">
+                                        <div class="text">${listItems[i].listname}</div>
+                                        <div>
+                                            <img id="trash" src="./img/icons/trash.svg" onclick="trashClicked(this)">
+                                        </div>
                                     </li>`);
     };
 };
 
 function trashClicked(el) {
-    $(el).parent().remove();
+    
+    listItems.splice($(el).parent().parent().index(), 1);
+    $(el).parent().parent().remove();
+    
+    printmypage();
         
     
 };
