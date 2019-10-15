0258a8a59e3f257700a8cdd2b476c982e0b3b961
Adakai <abovethenegativity23@gmail.com>
2019-10-14 21:21:17 -0600

    added bootstrap

--- js/index.js
+++ js/index.js
@@ -1,74 +1,90 @@
 // const listDisplay = document.getElementById('display_list');
 // const itemInput = document.getElementById('item_input');
 // const addItem = document.getElementById('add_item');
-const checked = './img/icons/checked.svg'
-const unchecked = './img/icons/unchecked.svg'
 
-const dateElement = document.getElementById('date');
+const checked = '<i class="fas fa-check-circle"></i>';
+const unchecked = '<i class="far fa-circle"></i>';
 
-const dateOptions = {weekday: 'long', month:'short', day:'numeric'};
+const dateElement = document.getElementById("date");
+
+const dateOptions = { weekday: "long", month: "short", day: "numeric" };
 const today = new Date();
 
-dateElement.innerHTML = today.toLocaleDateString('en-US', dateOptions);
+dateElement.innerHTML = today.toLocaleDateString("en-US", dateOptions);
 
 //jquery
 
 let listItems = [];
 
-$("#display_list").sortable({
-    stop: function(event, ui) {executecode();}
- });
+$("#list_name").sortable({
+    stop: function(event, ui) {
+        executecode();
+    }
+});
 
 function saveitem(event) {
-    switch(event.which) {
+    switch (event.which) {
         case 13:
             addItem();
             break;
         default:
-        }
-};
+    }
+}
 
 function buttonClicked() {
     addItem();
-};
+}
 
 function addItem() {
-    let input = $('#item_input').val();
+    let input = $("#item_input").val();
 
     let tempobj = {};
     tempobj.listname = input;
     tempobj.liscol = [];
     listItems.push(tempobj);
-    $('#item_input').val('');
+    $("#item_input").val("");
     printmypage();
     console.log(listItems);
-};
+}
 
 function printmypage() {
-    $('#display_list').html('');
-    for(var i = 0; i < listItems.length; i ++) {
-        $('#display_list').append(`<li class="item">
-                                        <img src=${unchecked}>
-                                        <div class="text">${listItems[i].listname}</div>
-                                        <div>
-                                            <img id="trash" src="./img/icons/trash.svg" onclick="trashClicked(this)">
-                                        </div>
-                                    </li>`);
-    };
-};
+    $("#list_name").html("");
+    for (var i = 0; i < listItems.length; i++) {
+        $("#list_name").append(`
+        
+        <li class="item">
+            <div id="edit_item" class="text" onclick="editItem()">
+                <h2>${listItems[i].listname}<h2>
+            </div>
+            <div class="item-btns">
+                <button type="button" class="btn btn-secondary" onclick="addTask()">Task<i class="fas fa-plus-circle"></i></button>
+                <button type="button" class="btn btn-secondary" onclick="trashClicked(this)">Delete<i class="fas fa-trash"></i></button>
+            </div>
+        </li>    
+        `);
+    }
+}
 
 function trashClicked(el) {
-    
     listItems.splice($(el).parent().parent().index(), 1);
     $(el).parent().parent().remove();
-    
+
     printmypage();
-        
-    
-};
+}
+
+function addTask() {
+    $("#edit_item").append(`
+        <input id="task_input" type="text" class="form-control" placeholder="CREATE A LIST" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" onkeyup="saveitem(event)">
+    `);
+}
+
+function editItem() {
+    $("#list_name").append(`
+        <input id="task_input" type="text" class="form-control" placeholder="CREATE A LIST" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" onkeyup="saveitem(event)">
+    `);
+}
 
 function executecode() {
-    let listarray = $("#display_list").children();
+    let listarray = $("#list_name").children();
     $(listarray[0]).addClass("activeitem");
-};
-
+}
